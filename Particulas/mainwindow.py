6e254570f928d27f2e49f509aa6048db6e3097aa
Manuel Aleman <many_98@outlook.es>
2020-02-20 07:38:39 -0600

    impresiòn de datos en TableWidget y PlainText

--- Particulas/mainwindow.py
+++ Particulas/mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QTableWidgetItem, QMessageBox
 from ui_mainwindow import Ui_MainWindow
 from PySide2.QtCore import Slot
 import json
@@ -8,6 +8,7 @@ import math
 class MainWindow(QMainWindow):
     # lista de Particulas
     particles = []
+    dis = 0
 
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -20,6 +21,9 @@ class MainWindow(QMainWindow):
 
         self.ui.actionAbrir.triggered.connect(self.abrir)
         self.ui.actionGuardar.triggered.connect(self.guardar)
+
+        self.ui.mostrar.clicked.connect(self.muestra_tabla)
+        self.ui.busca.clicked.connect(self.busqueda)
 
     def calculoEuclidiano(self, x_uno, y_uno, x_dos, y_dos):
         a = math.pow((x_dos - x_uno), 2)
@@ -37,6 +41,67 @@ class MainWindow(QMainWindow):
         self.ui.red.setValue(0)
         self.ui.green.setValue(0)
         self.ui.blue.setValue(0)
+
+    @Slot()
+    def busqueda(self):
+        particula = []
+
+        id = int(self.ui.busca_linea.text())
+
+        for item in self.particles:
+            if id == item['id']:
+                particula.append(item)
+
+        if len(particula) == 0:
+            QMessageBox.information(self, "Particulas", "No se encontro la particula :(")
+        else:
+            self.particulas_tabla(particula)
+
+    @Slot()
+    def muestra_tabla(self):
+        self.particulas_tabla(self.particles)
+
+    def particulas_tabla(self, particulas):
+        # limpiamos la tabla
+        self.ui.tabla.clear()
+
+        # cantidad de columnas que vamos a poner
+        self.ui.tabla.setColumnCount(10)
+        # numero de filas por tabla
+        self.ui.tabla.setRowCount(len(particulas))
+
+        # etiquetas para los headers de la tabla
+        labels = ['ID', 'OrigenX', 'OrigenY', 'DestinoX', 'DestinoY', 'Velocidad', 'Red', 'Green', 'Blue','Distancia']
+        self.ui.tabla.setHorizontalHeaderLabels(labels)
+
+
+        # contador de filas
+        row = 0
+        for particula in particulas:
+            id = QTableWidgetItem(str(particula['id']))
+            origenX = QTableWidgetItem(str(particula['origen']['x']))
+            origenY = QTableWidgetItem(str(particula['origen']['y']))
+            destinoX = QTableWidgetItem(str(particula['destino']['x']))
+            destinoY = QTableWidgetItem(str(particula['destino']['y']))
+            velocidad = QTableWidgetItem(str(particula['velocidad']))
+            r = QTableWidgetItem(str(particula['color']['red']))
+            g = QTableWidgetItem(str(particula['color']['green']))
+            b = QTableWidgetItem(str(particula['color']['blue']))
+            self.dis = self.calculoEuclidiano(particula['origen']['x'], particula['origen']['y'], particula['destino']['x'], particula['destino']['y'])
+            d = QTableWidgetItem(str(self.dis))
+
+            self.ui.tabla.setItem(row, 0, id)
+            self.ui.tabla.setItem(row, 1, origenX)
+            self.ui.tabla.setItem(row, 2, origenY)
+            self.ui.tabla.setItem(row, 3, destinoX)
+            self.ui.tabla.setItem(row, 4, destinoY)
+            self.ui.tabla.setItem(row, 5, velocidad)
+            self.ui.tabla.setItem(row, 6, r)
+            self.ui.tabla.setItem(row, 7, g)
+            self.ui.tabla.setItem(row, 8, b)
+            self.ui.tabla.setItem(row, 9, d)
+
+            row += 1
 
     @Slot()
     def add(self):
@@ -75,10 +140,17 @@ class MainWindow(QMainWindow):
     @Slot()
     def shows(self):
         print(" ")
+
         for i in self.particles:
             print(i)
-            print("Distancia Euclidiana: ", self.calculoEuclidiano(i['origen']['x'], i['origen']['y'],
-                                                                   i['destino']['x'], i['destino']['y']))
+            self.dis = self.calculoEuclidiano(i['origen']['x'], i['origen']['y'], i['destino']['x'], i['destino']['y'])
+            print(self.dis)
+
+            #insertamos llave y valor en el plainText
+            for x, v in i.items():
+                self.ui.salidaDatos.insertPlainText(x + ":" + str(v) + "\n")
+
+            self.ui.salidaDatos.insertPlainText("Distancia: " + str(self.dis) + '\n\n')
 
     @Slot()
     def abrir(self):
--- Particulas/ui_mainwindow.py
+++ Particulas/ui_mainwindow.py
@@ -20,128 +20,187 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(273, 432)
-        MainWindow.setStyleSheet(u"")
+        MainWindow.resize(669, 503)
+        MainWindow.setStyleSheet(u"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"\n"
+"")
         self.actionAbrir = QAction(MainWindow)
         self.actionAbrir.setObjectName(u"actionAbrir")
         self.actionGuardar = QAction(MainWindow)
         self.actionGuardar.setObjectName(u"actionGuardar")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.red = QSpinBox(self.centralwidget)
-        self.red.setObjectName(u"red")
-        self.red.setGeometry(QRect(92, 230, 131, 20))
-        self.red.setMaximum(255)
-        self.red.setValue(0)
-        self.green = QSpinBox(self.centralwidget)
-        self.green.setObjectName(u"green")
-        self.green.setGeometry(QRect(92, 256, 131, 20))
-        self.green.setMaximum(255)
-        self.blue = QSpinBox(self.centralwidget)
-        self.blue.setObjectName(u"blue")
-        self.blue.setGeometry(QRect(92, 282, 131, 20))
-        self.blue.setMaximum(255)
-        self.label_5 = QLabel(self.centralwidget)
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        self.tabWidget.setGeometry(QRect(20, 10, 631, 451))
+        self.tabWidget.setStyleSheet(u"background-color: rgb(255, 255, 255);")
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.muestra = QPushButton(self.tab)
+        self.muestra.setObjectName(u"muestra")
+        self.muestra.setGeometry(QRect(50, 380, 191, 23))
+        self.muestra.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
+"color: rgb(255, 255, 255);")
+        self.label_5 = QLabel(self.tab)
         self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(70, 260, 16, 16))
-        self.label_6 = QLabel(self.centralwidget)
-        self.label_6.setObjectName(u"label_6")
-        self.label_6.setGeometry(QRect(70, 280, 16, 16))
-        self.label_9 = QLabel(self.centralwidget)
-        self.label_9.setObjectName(u"label_9")
-        self.label_9.setGeometry(QRect(50, 10, 161, 21))
-        self.label_9.setStyleSheet(u"font: 75 16pt \"MS Shell Dlg 2\";\n"
-"color: rgb(0, 85, 0);\n"
-"color: rgb(44, 124, 22);")
-        self.label_9.setIndent(2)
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        self.widget.setGeometry(QRect(40, 50, 186, 171))
-        self.formLayout_2 = QFormLayout(self.widget)
+        self.label_5.setGeometry(QRect(50, 290, 16, 16))
+        self.green = QSpinBox(self.tab)
+        self.green.setObjectName(u"green")
+        self.green.setGeometry(QRect(110, 290, 131, 20))
+        self.green.setMaximum(255)
+        self.inserta = QPushButton(self.tab)
+        self.inserta.setObjectName(u"inserta")
+        self.inserta.setGeometry(QRect(50, 350, 191, 23))
+        self.inserta.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
+"color: rgb(255, 255, 255);")
+        self.blue = QSpinBox(self.tab)
+        self.blue.setObjectName(u"blue")
+        self.blue.setGeometry(QRect(110, 320, 131, 20))
+        self.blue.setMaximum(255)
+        self.label_8 = QLabel(self.tab)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(50, 220, 40, 19))
+        self.label_8.setStyleSheet(u"\n"
+"font: 11pt \"MS Reference Sans Serif\";")
+        self.label_4 = QLabel(self.tab)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(50, 260, 16, 16))
+        self.layoutWidget = QWidget(self.tab)
+        self.layoutWidget.setObjectName(u"layoutWidget")
+        self.layoutWidget.setGeometry(QRect(50, 50, 201, 164))
+        self.formLayout_2 = QFormLayout(self.layoutWidget)
         self.formLayout_2.setObjectName(u"formLayout_2")
         self.formLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.id = QLineEdit(self.widget)
+        self.id = QLineEdit(self.layoutWidget)
         self.id.setObjectName(u"id")
 
         self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.id)
 
-        self.label = QLabel(self.widget)
+        self.label = QLabel(self.layoutWidget)
         self.label.setObjectName(u"label")
 
         self.formLayout_2.setWidget(1, QFormLayout.LabelRole, self.label)
 
-        self.origen_X1 = QLineEdit(self.widget)
+        self.origen_X1 = QLineEdit(self.layoutWidget)
         self.origen_X1.setObjectName(u"origen_X1")
 
         self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.origen_X1)
 
-        self.label_2 = QLabel(self.widget)
+        self.label_2 = QLabel(self.layoutWidget)
         self.label_2.setObjectName(u"label_2")
 
         self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label_2)
 
-        self.origen_Y1 = QLineEdit(self.widget)
+        self.origen_Y1 = QLineEdit(self.layoutWidget)
         self.origen_Y1.setObjectName(u"origen_Y1")
 
         self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.origen_Y1)
 
-        self.label_3 = QLabel(self.widget)
+        self.label_3 = QLabel(self.layoutWidget)
         self.label_3.setObjectName(u"label_3")
 
         self.formLayout_2.setWidget(3, QFormLayout.LabelRole, self.label_3)
 
-        self.destino_X2 = QLineEdit(self.widget)
+        self.destino_X2 = QLineEdit(self.layoutWidget)
         self.destino_X2.setObjectName(u"destino_X2")
 
         self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.destino_X2)
 
-        self.label_7 = QLabel(self.widget)
+        self.label_7 = QLabel(self.layoutWidget)
         self.label_7.setObjectName(u"label_7")
 
         self.formLayout_2.setWidget(4, QFormLayout.LabelRole, self.label_7)
 
-        self.destino_Y2 = QLineEdit(self.widget)
+        self.destino_Y2 = QLineEdit(self.layoutWidget)
         self.destino_Y2.setObjectName(u"destino_Y2")
 
         self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.destino_Y2)
 
-        self.label_10 = QLabel(self.widget)
+        self.label_10 = QLabel(self.layoutWidget)
         self.label_10.setObjectName(u"label_10")
 
         self.formLayout_2.setWidget(5, QFormLayout.LabelRole, self.label_10)
 
-        self.velocidad = QLineEdit(self.widget)
+        self.velocidad = QLineEdit(self.layoutWidget)
         self.velocidad.setObjectName(u"velocidad")
 
         self.formLayout_2.setWidget(5, QFormLayout.FieldRole, self.velocidad)
 
-        self.label_11 = QLabel(self.widget)
+        self.label_11 = QLabel(self.layoutWidget)
         self.label_11.setObjectName(u"label_11")
 
         self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.label_11)
 
-        self.label_4 = QLabel(self.centralwidget)
-        self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(70, 230, 16, 16))
-        self.label_8 = QLabel(self.centralwidget)
-        self.label_8.setObjectName(u"label_8")
-        self.label_8.setGeometry(QRect(40, 210, 51, 16))
-        self.label_8.setStyleSheet(u"\n"
-"font: 11pt \"MS Reference Sans Serif\";")
-        self.inserta = QPushButton(self.centralwidget)
-        self.inserta.setObjectName(u"inserta")
-        self.inserta.setGeometry(QRect(80, 330, 111, 23))
-        self.inserta.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
+        self.red = QSpinBox(self.tab)
+        self.red.setObjectName(u"red")
+        self.red.setGeometry(QRect(110, 260, 131, 20))
+        self.red.setMaximum(255)
+        self.red.setValue(0)
+        self.label_6 = QLabel(self.tab)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(50, 320, 16, 16))
+        self.label_9 = QLabel(self.tab)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(73, 9, 107, 25))
+        self.label_9.setStyleSheet(u"font: 75 16pt \"MS Shell Dlg 2\";\n"
+"color: rgb(0, 85, 0);\n"
+"color: rgb(44, 124, 22);")
+        self.label_9.setIndent(2)
+        self.salidaDatos = QPlainTextEdit(self.tab)
+        self.salidaDatos.setObjectName(u"salidaDatos")
+        self.salidaDatos.setGeometry(QRect(300, 30, 291, 381))
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.gridLayout = QGridLayout(self.tab_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.busca_linea = QLineEdit(self.tab_2)
+        self.busca_linea.setObjectName(u"busca_linea")
+
+        self.horizontalLayout.addWidget(self.busca_linea)
+
+        self.busca = QPushButton(self.tab_2)
+        self.busca.setObjectName(u"busca")
+        self.busca.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
 "color: rgb(255, 255, 255);")
-        self.muestra = QPushButton(self.centralwidget)
-        self.muestra.setObjectName(u"muestra")
-        self.muestra.setGeometry(QRect(80, 360, 111, 23))
-        self.muestra.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
+
+        self.horizontalLayout.addWidget(self.busca)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+
+        self.tabla = QTableWidget(self.tab_2)
+        self.tabla.setObjectName(u"tabla")
+        font = QFont()
+        font.setFamily(u"MS Shell Dlg 2")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.tabla.setFont(font)
+        self.tabla.setStyleSheet(u"")
+        self.tabla.setAutoScrollMargin(19)
+        self.tabla.setTextElideMode(Qt.ElideMiddle)
+        self.tabla.setShowGrid(True)
+        self.tabla.setGridStyle(Qt.CustomDashLine)
+        self.tabla.horizontalHeader().setCascadingSectionResizes(False)
+
+        self.gridLayout.addWidget(self.tabla, 1, 0, 1, 1)
+
+        self.mostrar = QPushButton(self.tab_2)
+        self.mostrar.setObjectName(u"mostrar")
+        self.mostrar.setStyleSheet(u"background-color: rgb(44, 124, 22);\n"
 "color: rgb(255, 255, 255);")
+
+        self.gridLayout.addWidget(self.mostrar, 2, 0, 1, 1)
+
+        self.tabWidget.addTab(self.tab_2, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 273, 21))
+        self.menubar.setGeometry(QRect(0, 0, 669, 22))
         self.menuArchivo = QMenu(self.menubar)
         self.menuArchivo.setObjectName(u"menuArchivo")
         MainWindow.setMenuBar(self.menubar)
@@ -154,6 +213,9 @@ class Ui_MainWindow(object):
         self.menuArchivo.addAction(self.actionGuardar)
 
         self.retranslateUi(MainWindow)
+
+        self.tabWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -168,19 +230,23 @@ class Ui_MainWindow(object):
 #if QT_CONFIG(shortcut)
         self.actionGuardar.setShortcut(QCoreApplication.translate("MainWindow", u"Ctrl+S", None))
 #endif // QT_CONFIG(shortcut)
+        self.muestra.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"b", None))
-        self.label_6.setText(QCoreApplication.translate("MainWindow", u"g", None))
-        self.label_9.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">Part\u00edculas</span></p></body></html>", None))
+        self.inserta.setText(QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Color", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"r", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Origen X", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Origen Y", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Destino X", None))
         self.label_7.setText(QCoreApplication.translate("MainWindow", u"Destino Y", None))
         self.label_10.setText(QCoreApplication.translate("MainWindow", u"Velocidad", None))
         self.label_11.setText(QCoreApplication.translate("MainWindow", u"Id", None))
-        self.label_4.setText(QCoreApplication.translate("MainWindow", u"r", None))
-        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Color", None))
-        self.inserta.setText(QCoreApplication.translate("MainWindow", u"Agregar", None))
-        self.muestra.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"g", None))
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p align=\"center\"><span style=\" font-weight:600;\">Part\u00edculas</span></p></body></html>", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Agregar", None))
+        self.busca.setText(QCoreApplication.translate("MainWindow", u"Buscar", None))
+        self.mostrar.setText(QCoreApplication.translate("MainWindow", u"Mostrar", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tabla", None))
         self.menuArchivo.setTitle(QCoreApplication.translate("MainWindow", u"Archivo", None))
     # retranslateUi
 
