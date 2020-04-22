2d6c78aa79358e66d3d2f7d6edffd56e0dd9b35e
Manuel Aleman <many_98@outlook.es>
2020-04-22 15:09:24 -0500

    Recorrido en Anchura y Pofundidad

--- Particulas/Recorridos.py
+++ Particulas/Recorridos.py
@@ -22,10 +22,34 @@ class RecorridosG():
                 # añadimos vertice actual en visitados
                 self.visitados.append(actual)
 
+            # para cada vertice que el vertice actual tiene como destino
             for adyacente, peso in grafo[actual]:
+                # y que no ha sido visitado
                 if adyacente not in self.visitados:
                     # encolamos el vertice adyacente
                     self.cola.append(adyacente)
+
+    def profundidad(self, origen, grafo):
+        # se coloca el origen en la pila
+        self.pila.append(origen)
+        # y en la lista de visitados
+        self.visitadosP.append(origen)
+
+        # mientras la pila no este vacia
+        while self.pila:
+            # desapilar el vertice, sera el vertice actual
+            actual = self.pila.pop()
+            # imprimimos actual
+            print(actual)
+
+            # para cada vertice que el vertice actual tiene como destino
+            for adyacente, peso, in grafo[actual]:
+                # y que no ha sido visitado
+                if adyacente not in self.visitadosP:
+                    # apilamos vertices adyacentes
+                    self.pila.append(adyacente)
+                    # agregamos vertices adyacentes
+                    self.visitadosP.append(adyacente)
 
     """def profundidad(self, o, grafo):
         # Colocamos el origen en la pila
@@ -52,17 +76,3 @@ class RecorridosG():
                         self.pila.append(a)
                         print("Pila: " , self.pila)
     """
-
-    def profundidad(self, origen, grafo):
-
-        self.pila.append(origen)
-        self.visitadosP.append(origen)
-
-        while self.pila:
-            actual = self.pila.pop()
-            print(actual)
-
-            for adyacente, peso, in grafo[actual]:
-                if adyacente not in self.visitadosP:
-                    self.pila.append(adyacente)
-                    self.visitadosP.append(adyacente)
